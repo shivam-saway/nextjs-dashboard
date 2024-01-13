d0a3e3f75dab91c3516fd39c3ddb1650cf230246
shivam-saway <shivamsaway123@gmail.com>
2024-01-13 09:08:03 +0530

    removed set timeout

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -22,11 +22,9 @@ export async function fetchRevenue() {
     // console.log('Fetching revenue data...');
     // await new Promise((resolve) => setTimeout(resolve, 3000));
     noStore();
-    console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
